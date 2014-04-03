4852576a491aab13a057b8978b198fbc4b9a7507
Adam Levick <adlevick@gmail.com>
2014-04-03 12:31:55 -0400

    Added badge count method to badge model and added column to display badge count in student badge view

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -41,4 +41,9 @@ class Badge < ActiveRecord::Base
     earned_badges_by_student_id[[student.id]].try(:first)
   end
 
+   def earned_badge_count_for_student(student)
+      earned_badges.where(:student_id => student).count
+   end
+
+
 end
