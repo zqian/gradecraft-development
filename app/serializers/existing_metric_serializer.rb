f6af96aa684e7000e1b7452b095623cb643f869d
Mike Zazaian <mike@zop.io>
2014-04-17 09:58:32 -0400

    AJ-1573 Sort tiers by point value, then created order.

--- app/serializers/existing_metric_serializer.rb
+++ app/serializers/existing_metric_serializer.rb
@@ -1,4 +1,8 @@
 class ExistingMetricSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :max_points, :rubric_id, :order
   has_many :tiers, serializer: ExistingTierSerializer
+
+  def tiers
+    object.tiers.order("points DESC, created_at ASC")
+  end
 end
