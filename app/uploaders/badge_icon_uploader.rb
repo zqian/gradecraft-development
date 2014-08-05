4b86a0591dd492bc4d2fc55afedd230a169e3b09
Prabode Weebadde <prabode@venturit.com>
2014-08-05 12:27:12 -0400

    Commented storage option to use default fog storage, which s3.

--- app/uploaders/badge_icon_uploader.rb
+++ app/uploaders/badge_icon_uploader.rb
@@ -7,7 +7,8 @@ class BadgeIconUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #commenting out file to use default fog configuration, which uses s3
+  #storage :file
   
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
