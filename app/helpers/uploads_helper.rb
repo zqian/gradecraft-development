9f6f182a979aa8fc282b988df503198fb8afccd2
Caitlin Holman <caitlin.holman@gmail.com>
2014-11-05 06:09:50 -0500

    Fixed adding to score if current score is zero

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,9 +1,9 @@
 module UploadsHelper
   def upload_url(directory, model)
     course = "#{current_course.courseno}-#{current_course.id}"
-    course.gsub!(/[^0-9a-z]/i, '.').gsub!(/\.+/, '.')
+    course.gsub!(/[^0-9a-z]/i, '-').gsub!(/\.+/, '-')
     assignment = "#{model.name}-#{model.id}"
     assignment.gsub!(/[^0-9a-z]/i, '-').gsub!(/\.+/, '-')
-    return "uploads/submission_files/#{course}/#{assignment}/#{Time.new.strftime('%Y.%m.%d.%H.%M.%S')}/${filename}"
+    return "uploads/submission_files/#{course}/#{assignment}/#{Time.new.strftime('%Y-%m-%d-%H-%M-%S')}/${filename}"
   end
 end
