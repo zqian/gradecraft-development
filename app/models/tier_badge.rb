ad17ea0398576b4822d8ffb6e8489fbe6dffbe68
Mike Zazaian <mike@zop.io>
2014-12-02 08:12:38 -0500

    Add method for destroying duplicate tier badge from before proper validations were implemented.

--- app/models/tier_badge.rb
+++ app/models/tier_badge.rb
@@ -5,4 +5,9 @@ class TierBadge < ActiveRecord::Base
   attr_accessible :tier_id, :badge_id
 
   validates :tier_id, uniqueness: { scope: :badge_id }
+
+  def self.delete_duplicates
+    unique_ids = select("MIN(id) as id").group(:tier_id,:badge_id).collect(&:id)
+    where.not(id: unique_ids).destroy_all
+  end
 end
