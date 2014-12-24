597676687b9fd0041387f8fe99293a260b58406d
Caitlin Holman <caitlin.holman@gmail.com>
2014-12-24 16:08:08 -0500

    Removed gravatar and switched to uploaded avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,3 @@
 module UsersHelper
-  def gravatar_for(user, options = { })
-    gravatar_image_tag(user.email.downcase, :alt => h(user.name),
-                                            :class => 'img-rounded',
-                                            :gravatar => options)
 
-  end
 end
