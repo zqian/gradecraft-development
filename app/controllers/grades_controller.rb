e75e1112d78fccc86b1c7b89e0347b6504ad9507
Mike Zazaian <mike@zop.io>
2015-01-14 15:19:52 -0500

    fix issue where staff couldn't see full results on a student's graded rubric

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -9,6 +9,12 @@ class GradesController < ApplicationController
     @rubric = @assignment.rubric
     @metrics = @rubric.metrics
     @rubric_grades = serialized_rubric_grades
+
+    @viewable_rubric_grades = RubricGrade.joins("left outer join submissions on submissions.id = rubric_grades.submission_id").where(student_id: current_student.id).where(assignment_id: params[:assignment_id])
+    @comments_by_metric_id = @viewable_rubric_grades.inject({}) do |memo, rubric_grade|
+      memo.merge(rubric_grade.metric_id => rubric_grade.comments)
+    end
+
     if current_user_is_student?
       redirect_to @assignment
     end
