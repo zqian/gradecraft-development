fef9ad9052973906ad6cd47458e455a9533d18f6
Jonathan Gabel <hello@jonathangabel.com>
2015-01-23 14:55:05 -0500

    moved file upload into submission form

--- app/models/submission_file.rb
+++ app/models/submission_file.rb
@@ -1,8 +1,15 @@
 class SubmissionFile < ActiveRecord::Base
-  include S3File
+  #include S3File
 
   attr_accessible :filename, :filepath, :submission_id
 
   belongs_to :submission
-  before_save :strip_path
+  #before_save :strip_path
+
+  mount_uploader :filepath, SubmissionUploader
+
+
+  def url
+    filepath.url
+  end
 end
