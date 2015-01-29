02604d686a4dfb816dd14824b637767e23d61b02
Jonathan Gabel <hello@jonathangabel.com>
2015-01-29 11:47:18 -0500

    submission file url method for old and new

--- app/models/submission_file.rb
+++ app/models/submission_file.rb
@@ -1,22 +1,28 @@
 class SubmissionFile < ActiveRecord::Base
   include S3File
 
-  attr_accessible :filename, :filepath, :submission_id
+  attr_accessible :file, :filename, :filepath, :submission_id
 
   belongs_to :submission
 
-  validates :filename, presence: true,
-                         length: { maximum: 50 }
+  validates :filename, presence: true, length: { maximum: 50 }
 
   mount_uploader :file, SubmissionUploader
 
+  # Override S3File#url
+  # Legacy submission files were handled by S3 direct upload and we stored their
+  # Amazon key in the "filepath" field. Here we check if it has a value, and if
+  # so we use this to retrieve our secure url. If not, we use the path supplied by
+  # the carrierwave uploader
   def url
+    return file.url if Rails.env == "development"
+
     s3 = AWS::S3.new
     bucket = s3.buckets["gradecraft-#{Rails.env}"]
-    begin
+    if filepath.present?
       bucket.objects[CGI::unescape(filepath)].url_for(:read, :expires => 15 * 60).to_s #15 minutes
-    rescue Exception => e
-      bucket.objects[CGI::unescape(filepath.url.split("/").last)].url_for(:read, :expires => 15 * 60).to_s #15 minutes
+    else
+      bucket.objects[file.path].url_for(:read, :expires => 15 * 60).to_s #15 minutes
     end
   end
 end
