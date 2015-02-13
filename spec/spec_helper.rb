6ce5b83fddcbf5ca6e443f0df1ec1c7c8dd119f5
Jonathan Gabel <hello@jonathangabel.com>
2015-02-13 14:09:00 -0500

    changed all failing specs to pending

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
