bb301c6ac75d1f773e2a4995d47ebb5d15fd68c3
Max Langensiepen <mlangens@emich.edu>
2015-04-23 13:41:51 -0400

    checks for presence of student AJAX search element, prevents double page render for students

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,32 +1,34 @@
 !function(app, $) {
   $(document).ready(function() {
-    $.ajax({
-      url: $('.search-query').data('autocompleteurl'),
-      dataType: "json",
-      success: function(user) {
-        var users = user.students;
-        $('.search-query').omniselect({
-          source: users,
-          resultsClass: 'typeahead dropdown-menu',
-          activeClass: 'active',
-          itemLabel: function(user) {
-            return user.name;
-          },
-          itemId: function(user) {
-            return user.id;
-          },
-          renderItem: function(label) {
-            return '<li><a href="#">' + label + '</a></li>';
-          },
-          filter: function(item, query) {
-            return item.name.match(new RegExp(query, 'i'));
-          }
-        }).on('omniselect:select', function(event, id) {
-          $(event.target).val();
-          window.location = '/students/' + id;
-          return false;
-        });
-      }
-    });  
+    if($('.search-query').length) {
+      $.ajax({
+        url: $('.search-query').data('autocompleteurl'),
+        dataType: "json",
+        success: function(user) {
+          var users = user.students;
+          $('.search-query').omniselect({
+            source: users,
+            resultsClass: 'typeahead dropdown-menu',
+            activeClass: 'active',
+            itemLabel: function(user) {
+              return user.name;
+            },
+            itemId: function(user) {
+              return user.id;
+            },
+            renderItem: function(label) {
+              return '<li><a href="#">' + label + '</a></li>';
+            },
+            filter: function(item, query) {
+              return item.name.match(new RegExp(query, 'i'));
+            }
+          }).on('omniselect:select', function(event, id) {
+            $(event.target).val();
+            window.location = '/students/' + id;
+            return false;
+          });
+        }
+      });
+    }
   });
 }(Gradecraft, jQuery);
