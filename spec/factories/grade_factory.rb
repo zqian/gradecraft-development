18ce2f9eda45f803ec89e6e726fa6439990e54a4
Max Langensiepen <mlangens@emich.edu>
2015-04-23 17:36:41 -0400

    finishing view test for grading status

--- spec/factories/grade_factory.rb
+++ spec/factories/grade_factory.rb
@@ -8,5 +8,18 @@ FactoryGirl.define do
       #TODO: add minimum requirements to pass submission.graded?
       score { Faker::Number.number(5) }
     end
+
+    factory :unreleased_grade do
+      score { Faker::Number.number(5) }
+      status 'Graded'
+      after(:create) do |grade|
+        grade.assignment.update(release_necessary: true)
+      end
+    end
+
+    factory :in_progress_grade do
+      score { Faker::Number.number(5) }
+      status 'In Progress'
+    end
   end
 end
--- spec/views/info/grading_status_spec.rb
+++ spec/views/info/grading_status_spec.rb
@@ -1,19 +1,15 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe "info/grading_status", focus: true do
+describe "info/grading_status" do
 
-  before(:all) do
+  before(:each) do
     @title = "Unspecified Title"
     @course = create(:course)
     @team = create(:team, course: @course)
     @assignment_types = [create(:assignment_type, course: @course, max_value: 1000)]
     @assignment = create(:assignment, :assignment_type => @assignment_types[0])
     @student = create(:user)
-    @grades = create(:grades, course: @course, assignment: @assignment, student: @student)
-  end
-
-  before(:each) do
     assign(:assignment_types, @assignment_types)
     view.stub(:current_course).and_return(@course)
     view.stub(:current_student_data).and_return(StudentData.new(@student, @course))
@@ -21,20 +17,40 @@ describe "info/grading_status", focus: true do
     view.stub(:term_for).and_return("custom_term")
   end
 
-  describe "as student" do
+  describe "with ungraded submissions" do
     before(:each) do
-      #view.stub(:current_student).and_return(@student)
+      @grades = [create(:grade, course: @course, assignment: @assignment, student: @student)]
+      ungraded_submission = create(:submission, student: @student, assignment: @assignment)
+      @ungraded_submissions_by_assignment = [ungraded_submission].group_by(&:assignment)
+      @unreleased_grades_by_assignment = []
+      @in_progress_grades_by_assignment = []
+    end
+    it "renders" do
+      render
     end
   end
 
-  describe "as faculty" do
-    it "renders the instructor grade managment menu" do
-      view.stub(:current_user_is_staff?).and_return(true)
-      view.stub(:term_for).and_return("custom_term")
-      assign(:students, [@student])
-      assign(:assignment_grades_by_student_id, {@student.id => nil})
+  describe "with unreleased grades" do
+    before(:each) do
+      @grades = [create(:unreleased_grade, course: @course, assignment: @assignment, student: @student)]
+      @ungraded_submissions_by_assignment = []
+      @unreleased_grades_by_assignment = @grades.group_by(&:assignment)
+      @in_progress_grades_by_assignment = []
+    end
+    it "renders" do
       render
-      assert_select "li", text: "Grade", :count => 1
+    end
+  end
+
+  describe "with in progress grades" do
+    before(:each) do
+      @grades = [create(:in_progress_grade, course: @course, assignment: @assignment, student: @student)]
+      @ungraded_submissions_by_assignment = []
+      @unreleased_grades_by_assignment = []
+      @in_progress_grades_by_assignment = @grades.group_by(&:assignment)
+    end
+    it "renders" do
+      render
     end
   end
 end
